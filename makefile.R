ed4ead83dd1d325fbcdd080b468bb7d0fafa47ae
Pakillo <f.rodriguez.sanc@gmail.com>
2020-05-01 19:25:16 +0200

    add datos diarios municipios

--- makefile.R
+++ makefile.R
@@ -2,3 +2,30 @@
 fecha.distri <- as.Date("2020-04-30")
 
 rmarkdown::render("evolucion-coronavirus-andalucia.Rmd")
+
+
+## Datos municipios
+
+library(dplyr)
+
+muni.data <- readr::read_csv("datos/municipios.csv")
+
+if (!fecha.distri %in% muni.data$Fecha) {
+
+  munis.dia <- list.files("datos/municipios.dia/", pattern = ".csv", full.names = TRUE)
+
+  muni.dia <- purrr::map_df(munis.dia, readr::read_csv2) %>%
+    rename(Lugar = `Lugar de residencia`) %>%
+    dplyr::select(-X4) %>%
+    dplyr::filter(!is.na(Lugar), Medida != "Población") %>%
+    mutate(Medida = ifelse(Medida == "Confirmados total", "Confirmados", Medida)) %>%
+    mutate(Medida = ifelse(Medida == "Defunciones total", "Defunciones", Medida)) %>%
+    tidyr::pivot_wider(names_from = "Medida", values_from = "Valor") %>%
+    mutate(Fecha = fecha.distri) %>%
+    dplyr::select(Fecha, everything())
+
+  muni.data <- dplyr::bind_rows(muni.data, muni.dia) %>%
+    readr::write_csv(path = "datos/municipios.csv")
+
+}
+
