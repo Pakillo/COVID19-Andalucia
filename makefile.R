79c1eb0687ff3eccf96173b5467e54064b2c6bb9
Pakillo <f.rodriguez.sanc@gmail.com>
2020-05-07 23:44:50 +0200

    rearrange dataset municipios to include new columns provided by JA

--- makefile.R
+++ makefile.R
@@ -1,5 +1,5 @@
 
-fecha.distri <- as.Date("2020-05-04")
+fecha.distri <- as.Date("2020-05-06")
 
 rmarkdown::render("evolucion-coronavirus-andalucia.Rmd")
 
@@ -18,11 +18,19 @@ if (!fecha.distri %in% muni.data$Fecha) {
     rename(Lugar = `Lugar de residencia`) %>%
     dplyr::select(-X4) %>%
     dplyr::filter(!is.na(Lugar), Medida != "Población") %>%
-    mutate(Medida = ifelse(Medida == "Confirmados total", "Confirmados", Medida)) %>%
-    mutate(Medida = ifelse(Medida == "Defunciones total", "Defunciones", Medida)) %>%
+    #mutate(Medida = ifelse(Medida == "Confirmados total", "Confirmados", Medida)) %>%
+    #mutate(Medida = ifelse(Medida == "Defunciones total", "Defunciones", Medida)) %>%
+    mutate(Medida = ifelse(Medida == "Confirmado PCR", "Confirmados PCR", Medida)) %>%
+    mutate(Medida = ifelse(Medida == "Defunciones", "Defunciones total", Medida)) %>%
+    mutate(Medida = ifelse(Medida == "Confirmados Acumulados en 14 días",
+                           "Confirmados < 14 días", Medida)) %>%
     tidyr::pivot_wider(names_from = "Medida", values_from = "Valor") %>%
     mutate(Fecha = fecha.distri) %>%
-    dplyr::select(Fecha, everything())
+    dplyr::select(Fecha, Lugar, `Confirmados PCR`, `Confirmados PCR <14 días`,
+                  `Confirmados total`, `Confirmados < 14 días`, `Defunciones total`)
+
+  muni.dia %>%
+    assertr::verify(all.equal(names(muni.dia), names(muni.data)))
 
   muni.data <- dplyr::bind_rows(muni.data, muni.dia) %>%
     readr::write_csv(path = "datos/municipios.csv")
