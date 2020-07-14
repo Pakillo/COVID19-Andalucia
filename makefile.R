157b3e4e8d6c13a0a2ec5aab474378d00fc7c02d
Pakillo <f.rodriguez.sanc@gmail.com>
2020-07-15 00:20:48 +0200

    update 14 Julio

--- makefile.R
+++ makefile.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 ## Datos municipios
 
-fecha.munis <- as.Date("2020-07-12")
+fecha.munis <- as.Date("2020-07-13")
 
 muni.data <- readr::read_csv("datos/municipios.csv", guess_max = 50000)
 
