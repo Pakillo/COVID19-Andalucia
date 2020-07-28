bcd56e5532ec05ec6c300cac475f93a720a5ee04
Pakillo <f.rodriguez.sanc@gmail.com>
2020-07-28 14:17:53 +0200

    update 28 Julio

--- makefile.R
+++ makefile.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 ## Datos municipios
 
-fecha.munis <- as.Date("2020-07-26")
+fecha.munis <- as.Date("2020-07-27")
 
 muni.data <- readr::read_csv("datos/municipios.csv", guess_max = 50000)
 
@@ -22,8 +22,8 @@ if (!fecha.munis %in% as.Date(muni.data$Fecha)) {
     assertr::verify(unique(.$Medida) ==
                       c("Población", "Confirmados PCR",
                         "Confirmados PCR 14 días", "Confirmados PCR 7 días",
-                        "Total Confirmados", "Curados", "Fallecidos",
-                        "Confirmado PCR")) %>%
+                        "Total Confirmados", "Curados", "Fallecidos")) %>%
+                        #"Confirmado PCR")) %>%
     mutate(Medida = ifelse(Medida == "Confirmado PCR", "Confirmados PCR", Medida)) %>%
     mutate(Medida = ifelse(Medida == "Confirmados PCR 14 días", "ConfirmadosPCR14d", Medida)) %>%
     mutate(Medida = ifelse(Medida == "Total Confirmados", "Confirmados total", Medida)) %>%
