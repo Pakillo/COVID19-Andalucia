405131e6c4607d3bbe829cc71600ca6956f008e3
Pakillo <f.rodriguez.sanc@gmail.com>
2020-10-01 01:09:51 +0200

    update 30 sept

--- makefile.R
+++ makefile.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 ## Datos municipios
 
-fecha.munis <- as.Date("2020-09-28")
+fecha.munis <- as.Date("2020-09-29")
 
 
 muni.data <- readr::read_csv("datos/municipios.csv", guess_max = 50000)
@@ -55,5 +55,6 @@ if (!fecha.munis %in% as.Date(muni.data$Fecha)) {
 ## Render
 
 rmarkdown::render("evolucion-coronavirus-andalucia.Rmd")
+
 
 
