6c3fab5c7d8a035a5cd536a58b9cbc6e48d41bb3
Pakillo <f.rodriguez.sanc@gmail.com>
2020-10-21 18:43:12 +0200

    update 21 oct (sin actualizacion datos IECA)

--- makefile.R
+++ makefile.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 ## Datos municipios
 
-fecha.munis <- as.Date("2020-10-19")
+fecha.munis <- as.Date("2020-10-20")
 
 muni.data <- readr::read_csv("datos/municipios.csv",
                              col_types = "Dcccddddd",
