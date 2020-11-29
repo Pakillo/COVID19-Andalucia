193869c3558fbf015a1255874cc7002b6263258f
Pakillo <f.rodriguez.sanc@gmail.com>
2020-11-29 16:52:02 +0100

    update 27 nov

--- makefile.R
+++ makefile.R
@@ -1,8 +1,8 @@
 
 ## Especificar fecha de datos
-fecha.munis <- as.Date("2020-11-25")
+fecha.munis <- as.Date("2020-11-26")
 
-fecha.edad <- as.Date("2020-11-19")
+fecha.edad <- as.Date("2020-11-26")
 
 
 library(dplyr)
