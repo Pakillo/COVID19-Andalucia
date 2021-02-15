06c74f467174de808fa81d07af716fd838eecb43
Pakillo <f.rodriguez.sanc@gmail.com>
2021-02-15 01:12:53 +0100

    update 12 feb

--- makefile.R
+++ makefile.R
@@ -2,9 +2,9 @@
 library(dplyr)
 
 ## Especificar fecha de datos
-fecha.munis <- as.Date("2021-02-10")
+fecha.munis <- as.Date("2021-02-11")
 
-fecha.edad <- as.Date("2021-02-08")
+fecha.edad <- as.Date("2021-02-11")
 
 
 
