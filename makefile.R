f41a020def8ebd7b2f145a060b7de640e97d4bef
Pakillo <f.rodriguez.sanc@gmail.com>
2021-05-30 13:52:40 +0200

    update 30 may

--- makefile.R
+++ makefile.R
@@ -1,8 +1,8 @@
 
 ## Especificar fecha de datos
-fecha.munis <- as.Date("2021-05-26")
+fecha.munis <- as.Date("2021-05-27")
 
-fecha.edad <- as.Date("2021-05-20")
+fecha.edad <- as.Date("2021-05-27")
 
 
 
