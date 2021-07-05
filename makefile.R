50a5126fbeb67ec8630069bd4f9a754b5af7a612
Pakillo <f.rodriguez.sanc@gmail.com>
2021-07-05 13:35:29 +0200

    update 5 jul

--- makefile.R
+++ makefile.R
@@ -1,6 +1,6 @@
 
 ## Especificar fecha de datos
-fecha.munis <- as.Date("2021-07-01")
+fecha.munis <- as.Date("2021-07-04")
 
 fecha.edad <- as.Date("2021-07-01")
 
@@ -12,8 +12,7 @@ library(dplyr)
 
 muni.data <- readr::read_csv("datos/municipios.csv",
                              col_types = "Dcccddddd",
-                             guess_max = 50000) %>%
-  slice(1:241780)
+                             guess_max = 50000)
 
 munis <- readr::read_csv("datos/muni_prov_dist.csv")
 
